6a195746875ed9d31040d448c134b57a456264ed
Wasay Usmani <abdulusmani96@gmail.com>
2025-12-11 03:26:25 -0500

    trying slash for base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '', // change to your repo name if different
+  base: '/', // user/org site serves from root
 })
